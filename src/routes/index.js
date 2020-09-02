5206f944a6ed5a9e3c0dbba7747f5dedd0703cb2
My VO <kathy.nguyen198@gmail.com>
2020-09-02 16:07:41 +0200

    GET /api Renvoyer le message 'Hello, World'

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,4 +2,8 @@ const express = require('express');
 
 const mainRouter = express.Router();
 
+mainRouter.get('/', (req, res) => {
+  res.status(200).json({ message: 'Hello World!' });
+});
+
 module.exports = mainRouter;
