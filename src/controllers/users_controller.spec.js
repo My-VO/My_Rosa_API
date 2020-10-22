c46d64403e1e4f15959e609b856d7be1f542c2cf
My VO <kathy.nguyen198@gmail.com>
2020-10-22 19:04:13 +0200

    fix test Unit + Integration #addUser

--- src/controllers/users_controller.spec.js
+++ src/controllers/users_controller.spec.js
@@ -6,71 +6,53 @@ const db = require("../models");
 const usersController = require("./users_controller");
 
 const { Users } = db;
+const { userTDO } = require("../dto");
 
 describe("Controllers :: UsersController :: Integration", () => {
   describe("#addUser", () => {
-    it.skip("it should return the right object", async () => {
+    it("it should return the right object", async () => {
       // Given
       const data = {
         firstName: "My",
         lastName: "Rosa",
-        email: "myrosa@rosa.fr",
+        email: "rosamy@rosa.fr",
         password: "azertyazerty123",
       };
 
       // When
-      const newUser = await usersController.addUser(data);
+      const newUserDTO = await usersController.addUser(data);
 
       // Then
-      expect(newUser).to.have.property("first_name");
-      expect(newUser.first_name).to.equal(data.firstName);
-      expect(newUser).to.have.property("last_name");
-      expect(newUser.last_name).to.equal(data.lastName);
-      expect(newUser).to.have.property("email");
-      expect(newUser.email).to.equal(data.email);
-      expect(newUser).to.have.property("password");
+      expect(newUserDTO).to.have.property("first_name");
+      expect(newUserDTO.first_name).to.equal(data.firstName);
+      expect(newUserDTO).to.have.property("last_name");
+      expect(newUserDTO.last_name).to.equal(data.lastName);
+      expect(newUserDTO).to.have.property("email");
+      expect(newUserDTO.email).to.equal(data.email);
     });
   });
 });
 
 describe("Controllers :: UsersController :: Unit", () => {
   describe("#addUser", () => {
-    it("should execute hash password method", async () => {
+    it("should execute create method", async () => {
       // Given
       const data = {
-        userId: 2,
         firstName: "My",
         lastName: "Rosa",
-        email: "myrosa@com.vn",
+        email: "myrosa@com.fr",
         password: "azertyazerty123",
       };
 
-      const createStub = sinon.stub(bcrypt, "hash");
-
-      // When
-      await usersController.addUser(data);
-
-      // Then
-      expect(createStub.calledOnce).to.be.true;
-    });
-
-    it.skip("should execute create method", async () => {
-      // Given
-      const data = {
-        userId: 3,
-        firstName: "My",
-        lastName: "Rosa",
-        email: "myrosafr@com.fr",
-        // password: "azertyazerty123",
-      };
-
       const createStub = sinon.stub(Users, "create");
+      const createStubConvert2DTO = sinon.stub(userTDO, "convert2DTO");
 
       // When
       await usersController.addUser(data);
 
       // Then
       expect(createStub.calledOnce).to.be.true;
+      expect(createStubConvert2DTO.calledOnce).to.be.true;
     });
   });
 });
